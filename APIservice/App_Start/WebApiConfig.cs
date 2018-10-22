170d12704b455f61ba8064ccb32769b5bf0071f2
mohsen <talal1mohsen@outlook.com>
2018-10-22 21:52:39 +0300

    Last

---
+++ APIservice/App_Start/WebApiConfig.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Web.Configuration;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace APIservice
+{
+    public static class WebApiConfig
+    {
+        //public static void Register(HttpConfiguration config)
+        //{
+        //    // Web API configuration and services
+
+        //    // Web API routes
+        //    config.MapHttpAttributeRoutes();
+
+        //    config.Routes.MapHttpRoute(
+        //        name: "DefaultApi",
+        //        routeTemplate: "api/{controller}/{id}",
+        //        defaults: new { id = RouteParameter.Optional }
+        //    );
+        //}
+
+     
+        public static void Register(HttpConfiguration config)
+        {
+
+            config.EnableCors(new EnableCorsAttribute("http://localhost:3000", headers: "*", methods: "*"));
+
+            string isCORS = WebConfigurationManager.AppSettings["CORS_ENABLED"].ToString();
+            if (isCORS != "false")
+            {
+                string origins = WebConfigurationManager.AppSettings["CORS_ORIGINS"].ToString();
+                string headers = WebConfigurationManager.AppSettings["CORS_HEADERS"].ToString();
+                string methods = WebConfigurationManager.AppSettings["CORS_METHODS"].ToString();
+                string credentials = WebConfigurationManager.AppSettings["CORS_CREDENTIALS"].ToString();
+
+                var cors = new EnableCorsAttribute(origins, headers, methods);
+                cors.SupportsCredentials = (credentials != "false");
+                config.EnableCors(cors);
+
+
+               // Logger.LogMessage("CORS", "CORS ENABLED", "WEBAPI REGISTERATION");
+            }
+            else
+            {
+
+               // Logger.LogMessage("CORS", "CORS DISABLED", "WEBAPI REGISTERATION");
+            }
+
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("application/json"));
+
+            // Controller Only
+            // To handle routes like `/api/VTRouting`
+            // Controller Only
+            // To handle routes like `/api/VTRouting`
+            config.Routes.MapHttpRoute(
+                name: "ControllerOnly",
+                routeTemplate: "api/{controller}"
+            );
+
+            // Controller with ID
+            // To handle routes like `/api/VTRouting/1`
+            config.Routes.MapHttpRoute(
+                name: "ControllerAndId",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: null,
+                constraints: new { id = @"^\d+$" } // Only integers 
+            );
+
+            // Controllers with Actions
+            // To handle routes like `/api/VTRouting/route`
+            config.Routes.MapHttpRoute(
+                name: "ControllerAndAction",
+                routeTemplate: "api/{controller}/{action}"
+            );
+
+
+        }
+    }
+}
---
+++ APIservice/Controllers/EIPController.cs
@@ -0,0 +1,105 @@
+﻿using APIservice.Models;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Reflection;
+using System.Web;
+using System.Web.Http;
+
+namespace APIservice.Controllers
+{
+    public class EIPController : ApiController
+    {
+        [HttpGet]
+        public dynamic ping()
+        {
+            return "pong";
+        }
+        [HttpGet]
+        public dynamic getDocs()
+
+        {
+            return "Hi from Get DOcs";
+        }
+        [HttpPost]
+        public dynamic LogIn([FromBody] dynamic postParams)
+        {
+
+            string myJson = Convert.ToString(postParams);
+
+            var vals = JObject.Parse(myJson);
+
+            string username = vals["UserName"].ToString();
+            string password = vals["Password"].ToString();
+
+            EIPModel model1 = new EIPModel();
+            //call api
+            bool result = model1.LogIn(username, password);
+            if (result)
+
+              // return new HttpResponseMessage(HttpStatusCode.OK);
+               return "Exist";
+
+            else
+                return "Not Exist";
+
+        }
+
+        [HttpPost]
+        public string Register([FromBody] string postParams)
+        {
+            
+            string myJson = Convert.ToString(postParams);
+
+            var vals = JObject.Parse(myJson);
+
+            string username = vals["UserName"].ToString();
+            string password = vals["password"].ToString();
+            string Email = vals["Email"].ToString();
+            string Rights = vals["Rights"].ToString();
+
+            EIPModel model1 = new EIPModel();
+
+            string result = model1.Register(username, password, Email, Rights);
+            return result;
+        }
+
+        [HttpPost]
+        public string ReadDataFile([FromBody] string postParams)
+        {
+
+            string myJson = Convert.ToString(postParams);
+
+            var vals = JObject.Parse(myJson);
+
+            string FileName = vals["FileName"].ToString();
+            string FilePath = vals["FilePath"].ToString();
+            string UserName = vals["UserName"].ToString();
+            string Year = vals["Year"].ToString();
+            string Month = vals["Month"].ToString();
+            string Org = vals["Org"].ToString();
+            string UplodTime = vals["UplodTime"].ToString();
+
+
+            EIPModel model1 = new EIPModel();
+
+            string result = model1.GetDataFromExcel(FileName, FilePath, UserName,Year,Month, Org, UplodTime);
+
+           // MethodInfo mi = this.GetType().GetMethod("UploadFile");
+
+            return result;
+        }
+
+   
+        [HttpPost]
+        public string GetReport([FromBody] string postParams)
+        {
+            return "";
+        }
+    }
+}
